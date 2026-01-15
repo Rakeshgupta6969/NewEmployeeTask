08dcc8ae39dfaa3ad64b3c09e8f83445c268a1f1
Rakesh Gupta <rg688407@gmail.com>
2026-01-15 22:41:11 +0530

    updated

---
+++ src/Components/DashBoard/EmployeeDashBoard.jsx
@@ -0,0 +1,24 @@
+
+
+import React from "react";
+
+const  EmployeeDashBoard  = () =>{
+   
+    return (
+        
+        <Div>EmployeeDashBoard
+    
+      <h1> hi this is Rakesh Gupta</h1>
+
+
+
+        </Div>
+
+
+
+    )
+
+}
+
+
+export default EmployeeDashBoard;
